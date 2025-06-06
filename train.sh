a323dd695ca4725fcd50b9a3c42e166f5a6bde16
luoxue-star <lvjin1766@gmail.com>
2025-06-06 15:13:14 +0000

    Fix the training command bug

--- train.sh
+++ train.sh
@@ -2,6 +2,6 @@ exp_name_stage1=AniMerStage1
 exp_name_stage2=AniMerStage2
 experiment1=AniMerStage1
 experiment2=AniMertage2
-python main_amr.py exp_name=$exp_name_stage1 experiment=$experiment1 trainer=gpu launcher=local
+python main.py exp_name=$exp_name_stage1 experiment=$experiment1 trainer=gpu launcher=local
 cp -r ./logs/train/runs/$exp_name_stage1 ./logs/train/runs/$exp_name_stage2
-python main_amr.py exp_name=$exp_name_stage2 experiment=$experiment2 trainer=gpu launcher=local
+python main.py exp_name=$exp_name_stage2 experiment=$experiment2 trainer=gpu launcher=local
